2e86cb5bdba455afce706f5fb0b2a85f3fcf8dd5
unknown <meryemouali197@gmail.com>
2025-10-16 18:33:59 +0100

    Tp ReactNative01

--- App.js
+++ App.js
@@ -1,20 +1,54 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
-
-export default function App() {
-  return (
+import React from 'react';
+import {Image,StyleSheet, View, Text} from 'react-native';
+export default function App(){
+  return(
+    <>
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Image source={require('./assets/emsi.jpg')}
+      style={styles.logo}
+      resizeMode='contain'/>
+      <Text style={styles.schoolName}>EMSI CENTRE</Text>
     </View>
+    <View style={styles.info}>
+        <Text style={styles.label}>Nom:<Text style={styles.value}>OUALI</Text></Text>
+        <Text style={styles.label}>Prénom:<Text style={styles.value}>MERYEM</Text></Text>
+        <Text style={styles.label}>Année universitaire:<Text style={styles.value}>2025/2026</Text></Text>
+      </View>
+      </>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+const styles=StyleSheet.create({
+  container:{
+    flex:1,
+    justifyContent:'center',
+    alignItems:'center',
+    backgroundColor:"#f6f7fb",
+    flexDirection:'row',
+    justifyContent:'space-around',
   },
+  logo:{
+    width:200,
+    height:200,
+  },
+  schoolName: {
+    fontSize: 22,
+    fontWeight: 'bold',
+    color: '#1f2937',
+    marginTop: 20, // espace entre image et texte
+    letterSpacing: 1,
+},
+info: {
+alignItems: 'center',
+gap: 8,
+},
+label: {
+fontSize: 16,
+color: '#6b7280',
+fontWeight: '500',
+},
+value: {
+fontSize: 17,
+color: '#111827',
+fontWeight: 'bold',
+},
 });
